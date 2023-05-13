28585cf26eb6086606cfa5dcd3c0c4a3e0ac59b2
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-13 18:30:20 +0530

    last Commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import NavBar from './Components/NavBar/NavBar'
 import "./App.css"
 import Banner from './Components/Banner/Banner'
 import RowPost from './Components/RowPost/RowPost'
+import {originals,actions, comedy, horror, romance, trending} from "./urls"
 
 function App() {
   return (
     <div>
     <NavBar/>
     <Banner/>
-    <RowPost/>
+    <RowPost urls={originals} title="Netflix Originals"/>
+    <RowPost urls={trending} title="Trending" isSmall/>
+    <RowPost urls={actions} title="Action" isSmall/>
+    <RowPost urls={comedy} title="Comedy" isSmall/>
+    <RowPost urls={romance} title="Romance" isSmall/>
+    <RowPost urls={horror} title="Horror" isSmall/>
     </div>
   )
 }
