795811c027028974355c6c4db51684369256e74a
Abhinav <abhinavkinarullathil@gmail.com>
2023-05-15 10:05:12 +0530

    Changes in banner and funcionality

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -8,13 +8,13 @@ function Banner() {
     useEffect(() => {
       axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
         console.log(response.data.results[0]);
-        setMovie(response.data.results[0])
+        setMovie(response.data.results[1])
       })
     }, [])
     
   return (
     <div 
-    style={{backgroundImage :`url(${movie ? imageUrl+movie.backdrop_path :" "})` }}
+    style={{backgroundImage :`url(${movie ? imageUrl+movie.backdrop_path :""})` }}
     className='banner'>
       <div className='content'>
         <h1 className='title'>{movie ? movie.title : " "} </h1>
--- src/Components/RowPost/RowPost.js
+++ src/Components/RowPost/RowPost.js
@@ -29,7 +29,6 @@ function RowPost(props) {
         console.log(id);
         axios.get(`/movie/${id}/videos?api_key=${API_KEY}&language=en-US`).then((response)=>{
            if(response.data.results.length !==0){
-            console.log("podooooo");
             setUrlId(response.data.results[0])
            }else{
             console.log("Array Empty");
@@ -41,7 +40,10 @@ function RowPost(props) {
       <h2>{props.title}</h2>
       <div className='posters'>
         {movies.map((obj)=>
+        <div>
             <img onClick={()=>handleMovie(obj.id)} className={props.isSmall ?"smallPoster " :'poster'} src={`${imageUrl+obj.backdrop_path}`} alt="post" /> 
+            <p style={{marginTop:"0.7rem"}}>{obj.title}</p>
+            </div>
         )}
 
       </div>
